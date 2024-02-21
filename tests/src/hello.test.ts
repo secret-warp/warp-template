cee73071588b75b60e9829d26737c66d99381665
Reyth3 <mrreynevan2@gmail.com>
2024-02-21 13:44:58 +0100

    bump nodejs deps and added new util

--- tests/src/hello.test.ts
+++ tests/src/hello.test.ts
@@ -1,30 +1,48 @@
-import { expect } from 'chai';
-import { SecretNetworkClient } from 'secretjs';
-import { getGenesisWallets, getLocalSecretConnection, storeAndInitContract } from './utils/localsecret'
+import { expect } from "chai";
+import { SecretNetworkClient, Wallet } from "secretjs";
+import {
+  getGenesisWallets,
+  getLocalSecretConnection,
+  requestFaucetForAddress,
+  storeAndInitContract,
+} from "./utils/localsecret";
 
-describe('Hello World', () => {
-    
-    let client: SecretNetworkClient;
-    
-    before(() => {
-        client = getLocalSecretConnection();
-    })
+describe("Hello World", () => {
+  let client: SecretNetworkClient;
 
-    it('Can get current block height', async () => {
-        const block = await client.query.tendermint.getLatestBlock({});
-        expect(block.block_id.hash).to.not.be.null;
-    });
+  before(() => {
+    client = getLocalSecretConnection();
+  });
 
-    it("Has correct account balance", async () => {
-        const [a, b, c, d] = getGenesisWallets();
-        let balance = await client.query.bank.balance({ address: a.address, denom: "uscrt" });
-        expect(parseInt(balance.balance.amount)).to.be.gte(99 * 1e6);
+  it("Can get current block height", async () => {
+    const block = await client.query.tendermint.getLatestBlock({});
+    expect(block.block_id.hash).to.not.be.null;
+  });
+
+  it("Has correct account balance", async () => {
+    const [a, b, c, d] = getGenesisWallets();
+    let balance = await client.query.bank.balance({
+      address: a.address,
+      denom: "uscrt",
     });
-    it("Correctly instantiates a contract", async() => {
-        let [a] = getGenesisWallets();
-        let signingClient = getLocalSecretConnection(a);
-        let address = await storeAndInitContract(signingClient, "./contract.wasm", {}, "New Contract "+Date.now());
-        console.log(address);
-        expect(address).to.be.not.eq("");
-    });
+    expect(parseInt(balance.balance.amount)).to.be.gte(99 * 1e6);
+  });
+  it("Correctly instantiates a contract", async () => {
+    let [a] = getGenesisWallets();
+    let signingClient = getLocalSecretConnection(a);
+    let address = await storeAndInitContract(
+      signingClient,
+      "./contract.wasm",
+      {},
+      "New Contract " + Date.now()
+    );
+    console.log(address);
+    expect(address).to.be.not.eq("");
+  });
+  
+  it("Can request faucet", async () => {
+    let randomWallet = new Wallet();
+    let address = randomWallet.address;
+    let success = await requestFaucetForAddress(address);
+    expect(success).to.be.true;
 });
